95148122d0542ea0ecd3563eb97a899cec433d87
L3nnyK <32324077+L3nnyK@users.noreply.github.com>
2022-07-11 22:55:39 +0800

    Bulk of code is in.
    Now to test.

--- Leonard.sh
+++ Leonard.sh
@@ -8,37 +8,37 @@ function initialise()
 {
 	#Add in apps as needed, assume nmap, ssh, whois are all not installed.
 	
-	apt update && apt install -y nmap ssh whois openssh-client tor sshpass
+	sudo apt update && apt install -y nmap ssh whois openssh-client tor sshpass
 	  
 }
 
 function nipemeup()
 {
-	cd /home/kali
+	cd ~
 	echo "Setting current location to:"
 	pwd
-	
+		
 # Download the repository
 	git clone https://github.com/htrgouvea/nipe && cd nipe
 
 # Install libs and dependencies
-	echo "Yes" | cpan install Try::Tiny Config::Simple JSON
+	echo "Yes" | sudo cpan install Try::Tiny Config::Simple JSON
 
  # Install Nipe, must be run as root.
  sudo perl nipe.pl install
 	
 }
-echo "Getting your system ready. Please ensure the script is run with sudo permission"
- 
-initialise && nipemeup
+echo "Getting your system ready."
+echo  
+echo "Initialising"
+initialise
+echo
+echo "Proceeding to get Nipe setup."
+nipemeup
 
 ###########################################################################
 
 #Get initial IP address and country. Setup the variables necessary for later.
-
-# Included this line in case nipe is already running, the if loop will return that country of origins match. So we stop nipe before restarting it."
-cd ~/nipe
-sudo perl nipe.pl stop
 
 function Originchk()
 {
@@ -100,15 +100,15 @@ fi
 #~ scans and whois queries.
 #~ Available tools: sshpass, ssh
 
-#AWS Ubuntu Test Environment ec2-13-250-38-111.ap-southeast-1.compute.amazonaws.com
+#Digital Ocean Ubuntu Test Environment 128.199.179.192
 #User root
-#Password LkNRTestB0X!
+#Password LkNRTest!B0X
 
-echo "You are all set. Connecting you to ec2-13-250-38-111.ap-southeast-1.compute.amazonaws.com"
+echo "You are all set. Connecting you to 128.199.179.192"
 
-sshpass -p 'LkNRTestB0X!' ssh root@ec2-13-250-38-111.ap-southeast-1.compute.amazonaws.com 'apt update && apt install -y nmap ssh sshpass whois && nmap -oA scanresults -Pn $(curl ipinfo.io/ip) && whois $(cat scanresults.gnmap | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" | uniq)
-'
+sshpass -p 'LkNRTest!B0X' ssh -o StrictHostKeyChecking=no root@128.199.179.192 'apt update && apt install -y nmap ssh sshpass whois && nmap -oA scanresults -Pn $(curl ipinfo.io/ip) && whois $(cat scanresults.gnmap | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" | uniq)'
 
-
-
+#Get the outputs thank you very much.
+echo "Getting you your files, please check ~/ for scanresults.gnmap and whoisresults files."
+sshpass -p 'LkNRTest!B0X' scp -v root@128.199.179.192:"scanresults.gnmap whoisresult" ~/    
 
