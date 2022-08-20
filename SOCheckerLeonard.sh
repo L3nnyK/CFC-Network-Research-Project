973fcaf01768e218a5028baaacc473030ea1c40a
L3nnyK <32324077+L3nnyK@users.noreply.github.com>
2022-08-20 08:40:49 -0400

    SOChecker project framework upload.

---
+++ SOCheckerLeonard.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+##Objective
+#Create a script that runs different cyber attacks in a given network to check if monitoring alerts appear.
+
+#1. Install relevant application on local computer.
+
+#2. Execute network scans and attacks. Allow the user to choose two methods of scanning and two different network attacks to run via your script.
+# Available tools: nmap, masscan, msfconsole
+
+
+#3. Log executed Attacks
+#Every scan or attack should be logged and saved with the date and used arguments.
