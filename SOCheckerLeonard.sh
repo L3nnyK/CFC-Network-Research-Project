a2b3c54eb4684f870c8832d5e5d838230cecb4a6
L3nnyK <32324077+L3nnyK@users.noreply.github.com>
2022-08-24 14:26:44 +0800

    Updates to the initial script.

--- SOCheckerLeonard.sh
+++ SOCheckerLeonard.sh
@@ -5,9 +5,25 @@
 
 #1. Install relevant application on local computer.
 
+function inst()
+{
+sudo apt-get update && sudo apt-get install -y nmap massscan msfconsole
+}
+
 #2. Execute network scans and attacks. Allow the user to choose two methods of scanning and two different network attacks to run via your script.
 # Available tools: nmap, masscan, msfconsole
 
+function  chkme()
+{
+#allow the user to choose different scans and attacks
 
+}
+
+function LOGIT()
+
+{
+#use this function to save results such as date, time, IPs, kind of attack.
+
+}
 #3. Log executed Attacks
 #Every scan or attack should be logged and saved with the date and used arguments.
