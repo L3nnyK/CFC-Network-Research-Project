b6d40486f75bf85b63b75a3fed15db1319ee2620
L3nnyK <32324077+L3nnyK@users.noreply.github.com>
2022-09-05 08:06:41 -0400

    Folder cleanup

--- Python Projects/pyscript.py
+++ Python Projects/pyscript.py
@@ -18,15 +18,36 @@
 # ~ 7. Print out your age in a sentence that starts with ‘My age this year is: ‘ by using the str() to typecast your age
     # ~ - Hint
     
-birth_year=1984
-This_year=2022
-age=This_year-birth_year
-print(age)
-print('My age this year is: ', str(age))
+# ~ birth_year=1984
+# ~ This_year=2022
+# ~ age=This_year-birth_year
+# ~ print(age)
+# ~ print('My age this year is: ', str(age))
 
-# Get an input from the user the birth year and store it in a variable
+# ~ # Get an input from the user the birth year and store it in a variable
 
-birth_year = input("Please tell me which year you are born in:")
 
-print(birth_year)
-print(type(birth_year))
+# ~ birth_year = input("Please tell me which year you are born in:")
+# ~ print(birth_year)
+# ~ print(type(birth_year))
+
+
+
+# ~ # Create a loop that runs 3 times
+# ~ # Get name input from user and store in variable
+
+# ~ for inputvar in range(3):
+	# ~ usrname=input("Please give me your name: ")
+	# ~ print(usrname)
+
+#Do it in a script
+#1. Create an empty list
+#2. Create a loop the runs 3 times
+#3. Ask the user to enter a names and add it to the list.
+#4. print the list AFTER the 3 times i done.
+
+namelist=[]
+for listcounter in range(3):
+	usrname = input("Please give me a name: ")
+	namelist.append(usrname)
+print(namelist)
