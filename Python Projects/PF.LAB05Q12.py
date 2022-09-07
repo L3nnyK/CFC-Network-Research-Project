143fac8f01c137f016d85ee6954a5991020ef3aa
L3nnyK <32324077+L3nnyK@users.noreply.github.com>
2022-09-07 07:15:12 -0400

    Project files

---
+++ Python Projects/PF.LAB05Q12.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+#12. Use the file users.db and find the students average score. 
+
+scores=[]
+file=open('users.db','r')
+data=file.readlines()
+print(data)
+
+sdata=[]
+for clean in data:
+	sdata.append(clean.strip())
+
+print(sdata)
+result=[]
+for ii in sdata:
+	result.append(int(ii.split('\t')[1]))
+
+print(result)
+sum=0
+for val in result:
+	sum= sum + val
+	
+print("Total is : " + str(sum))
+
+nummer=len(result)
+print("Number of of studnets :" + str(nummer))
+
+Average=sum/nummer
+
+print("Average is : " + str(Average))
+
+file.close()
+
